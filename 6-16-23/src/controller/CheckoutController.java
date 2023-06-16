d18799cb3c7838284b920d0f0728356cf30bbb44
Cyrah Sophia Angella Cuenca <119916620+i675nrk@users.noreply.github.com>
2023-06-16 09:43:02 +0800

    updated layout, added new buttons

---
+++ 6-16-23/src/controller/CheckoutController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class CheckoutController implements Initializable{
+
+    @FXML
+    VBox myvbox;
+
+    @FXML
+    Pane pane1, pane2, pane3, pane4, pane5, pane6;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+       
+    }
+
+
+}
